21e6ffb0ecde5d98aa78af1f6d521f61eacfaee4
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-03 11:01:34 -0400

    Create iptables-reset.sh

---
+++ iptables-reset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+iptables -P INPUT ACCEPT;
+iptables -P OUTPUT ACCEPT;
+iptables -P FORWARD ACCEPT;
+iptables -F;
+iptables -F DENYIN;
+iptables -F DENYOUT;
+iptables -F INVALID;
+iptables -F LOGDROPIN;
+iptables -F LOGDROPOUT;
+iptables -F LOCALINPUT;
+iptables -F LOCALOUTPUT;
+iptables -F INVDROP;
+iptables -X;
+iptables -Z;
+iptables -A OUTPUT -p tcp ACCEPT;
+iptables -A INPUT -p tcp ACCEPT;
+iptables-save;
